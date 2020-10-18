9d0391e319c7eb6c2e05e1215390f9abbd16040d
bucephalus41 <bucephalus41@gmail.com>
2020-10-18 19:44:22 +0900

    ㅇㅇㅇ

--- app/src/main/java/com/zzazzu/newintent_201017/MainActivity.kt
+++ app/src/main/java/com/zzazzu/newintent_201017/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.zzazzu.newintent_201017
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherActivityBtn.setOnClickListener {
+
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
+
+
     }
 }
