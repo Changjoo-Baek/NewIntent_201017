65a0b4a08ac485637f8ee96a844aed3457145f19
bucephalus41 <bucephalus41@gmail.com>
2020-10-18 21:22:36 +0900

    완성.
    intent, 등 각종 기능 확인

--- app/src/main/java/com/zzazzu/newintent_201017/MainActivity.kt
+++ app/src/main/java/com/zzazzu/newintent_201017/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.zzazzu.newintent_201017
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity() {
+class MainActivity : AppCompatActivity() {
+
+    val REQUEST_FOR_NICKNAME = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,11 +38,75 @@ class MainActivity : BaseActivity() {
 
             val myIntent=Intent(this,EditNickNameActivity::class.java)
 
-            startActivityforResult(myIntent, REQUEST_FOR_NICKNAME)
+            startActivityForResult(myIntent, REQUEST_FOR_NICKNAME)
 
         }
 
+        dialBtn.setOnClickListener {
 
+            val inputPhoneNum = phoneNumEdt.text.toString()
+
+            val myUri = Uri.parse("tel:$(inputPhoneNum)")
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+            startActivity(myIntent)
+
+        }
+
+        callBtn.setOnClickListener {
+            val inputPhoneNum = phoneNumEdt.text.toString()
+
+            val myUri = Uri.parse("tel:${inputPhoneNum}")
+            val myIntent = Intent(Intent.ACTION_CALL, myUri)
+            startActivity(myIntent)
+
+
+        }
+
+        snsBtn.setOnClickListener {
+
+            val inputPhoneNum = phoneNumEdt.text.toString()
+
+            val myUri = Uri.parse("smsto:${inputPhoneNum}")
+            val myIntent = Intent(Intent.ACTION_SENDTO, myUri)
+
+            myIntent.putExtra("sms_body", "[공유] 이 앱을 다운받아 주세요!")
+
+            startActivity(myIntent)
+
+        }
+
+        naverWebinkBtn.setOnClickListener {
+
+            val myUri = Uri.parse("https://naver.com")
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+            startActivity(myIntent)
+
+        }
+
+        kakaoPlayStoreLinkBtn.setOnClickListener {
+
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+            startActivity(myIntent)
+
+        }
 
     }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_FOR_NICKNAME) {
+
+            if(resultCode == Activity.RESULT_OK) {
+
+                val newNickName = data?.getStringExtra("nick")
+
+                nickNameTxt.text = newNickName
+
+            }
+        }
+    }
+
 }
